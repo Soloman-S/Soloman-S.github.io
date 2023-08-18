7fde2cafd31f71ba040f72bd1459c8c9e0cc220e
Soloman-S <72734751+Soloman-S@users.noreply.github.com>
2023-08-19 05:16:32 +1000

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-
-
-const listEl = document.querySelector('.listContainer');
-
-for each file in images
-  const imgEl = document.createElement('img');
-  imgEl.classList.add('imageContainer');
-  imgEl.src = imagePath;
-  listEl.appendChild(imgEl);
