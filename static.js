a121bac68c2b017187da30319fa067823af30729
Soloman-S <72734751+Soloman-S@users.noreply.github.com>
2023-08-19 11:05:50 +1000

    Update static.js

--- static.js
+++ static.js
@@ -25,7 +25,8 @@ window.addEventListener('DOMContentLoaded', () => {
   for (b of buttonArray) {
     const butEl = document.createElement('button');
     butEl.classList.add('btn');
-    butEl.addEventListener('click', function(){filterSelection(b.className)});
+    butEl.onclick = function(){filterSelection(b.className)}
+    //butEl.addEventListener('click', function(){filterSelection(b.className)});
     butEl.innerHTML = b.label;
     listEl.appendChild(butEl);
   }
