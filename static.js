84f53aea4374f90a3da635f6204ed408881e6dcd
Soloman-S <72734751+Soloman-S@users.noreply.github.com>
2023-08-19 10:23:25 +1000

    Update static.js

--- static.js
+++ static.js
@@ -25,7 +25,7 @@ window.addEventListener('DOMContentLoaded', () => {
   for (b of buttonArray) {
     const butEl = document.createElement('button');
     butEl.classList.add('btn');
-    butEl.onclick = "filterSelection('class1')";
+    butEl.onclick = filterSelection(b.className);
     listEl.appendChild(butEl);
   }
   
